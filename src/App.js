483c2fc34ac97a9bbb158fe906b1284699b9570c
AJ <alexanderjdiaz89@gmail.com>
2020-04-24 23:20:58 -0600

    add main-nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  state = {
+    stuff: [],
+  }
+  
+
+
+  render() {
+    return (
+      <div className="page-wraper">
+        <nav className="main-nav">
+          <h1>This is the main nav bar</h1>
+        </nav>
+        <main>
+
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
